4fa565ecebeb9385cbc8b3999227062c6e245e30
annjeaner <annjeaner@gmail.com>
2026-05-14 22:07:53 +0200

    Update App.js
    fixed namepath to favicons

--- src/App.js
+++ src/App.js
@@ -62,8 +62,8 @@ meta({ name: "twitter:description", content: SITE.description });
 meta({ name: "twitter:image",       content: SITE.image });
 link({ rel: "canonical", href: SITE.url });
 link({ rel: "icon",             type: "image/x-icon",  href: "/favicon.ico" });
-link({ rel: "icon",             type: "image/png", sizes: "32x32", href: "/favicon-32.png" });
-link({ rel: "icon",             type: "image/png", sizes: "16x16", href: "/favicon-16.png" });
+link({ rel: "icon",             type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" });
+link({ rel: "icon",             type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" });
 link({ rel: "apple-touch-icon", sizes: "180x180",  href: "/apple-touch-icon.png" });
 const schemas = [
 {
